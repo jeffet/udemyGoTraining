3f9b36d06c772f1ad383ab8174bf27a94f322c0e
Jeff Tennenbaum <tennenjl@us.ibm.com>
2016-12-24 21:50:04 -0500

    Added 02_Package code

---
+++ 02_package/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/jeffet/udemyGoTraining/02_package/stringutil"
+
+func main() {
+	fmt.Println(stringutil.Reverse("!ffeJ olleH"))
+	fmt.Println(stringutil.MyName)
+}
